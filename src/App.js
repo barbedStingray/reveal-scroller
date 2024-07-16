3e86c4c730662bd9f759b87ec48700072a1feeca
Ben Rehmann <brehmann@me.com>
2024-07-16 09:57:59 -0500

    images added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+import fruits from './Images/fruits.jpg';
+import cherries from './Images/cherries.jpg';
+import smoothie from './Images/smoothie.jpg';
+import popcorn from './Images/popcorn.jpg';
+
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Reveal Scroller</h1>
+
+      <div className='revealScroller'>
+
+        <div className='scroller'>
+          <p>Fruits</p>
+          <p>Cherries</p>
+          <p>Smoothie</p>
+          <p>Popcorn</p>
+
+        </div>
+        <div className='reveal'>
+
+        </div>
+
+      </div>
+
+
     </div>
   );
 }
